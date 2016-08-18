663ab7a8a7e4994191c19d245a0e2cead5a041a2
Rush Tehrani <rush.tehrani@gmail.com>
2016-08-18 07:24:17 -0700

    serve bundle.js from memory

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
-const webpack = require('webpack'),
-      path = require('path');
+const path = require('path');
 
-const BUILD_DIR = path.resolve(__dirname, 'public');
 const APP_DIR = path.resolve(__dirname, 'app');
 
 const config = {
   entry: APP_DIR + '/index.jsx',
   output: {
-    path: BUILD_DIR + '/js',
+    publicPath: '',
     filename: 'bundle.js'
   },
   module : {
